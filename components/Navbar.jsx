efe95056f26a7ab9030c31256b9ff442c7c3095a
Muhammad Zain-ul-abideen <zaynzafar18@gmail.com>
2022-12-21 22:31:05 +0500

    initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,67 @@
+import {useState} from 'react'
+import {close ,logo, menu} from '../assets'
+import {navLinks} from '../constants'
+import {Link} from "react-router-dom"
+
+const Navbar = () => {
+
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
+
+  const state = "loggedin";
+
+  return (
+    <div>
+        <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+      {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? "text-white" : "text-dimWhite"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+          >
+            <Link to={`${nav.id}`} onClick={()=>{
+              setActive(nav.title)
+            }}>{nav.title}</Link>
+          </li>
+        ))}
+      </ul>
+      
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+        />
+
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <Link to={`${nav.id}`}>{nav.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+     </nav>
+    </div>
+    
+  )
+}
+
+export default Navbar
